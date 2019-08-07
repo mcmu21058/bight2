0cec9137079f26633ec28be36de810c93a695309
Dana <28915073+mcmu21058@users.noreply.github.com>
2019-08-06 23:30:13 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+     Navbar,
+     Nav,
+     NavItem,
+     NavLink,
+     NavbarBrand,
+     NavbarToggler,
+     Collapse
+}
+from "reactstrap";
+function Header() {
+     return(
+          <div>
+          <Navbar color = "dark" dark fixed="top" expand="sm">
+          <NavbarBrand href ="/">
+          <span className = "text-warning" >Better Bus App </span> 
+           </NavbarBrand> 
+           <NavbarToggler onclick = {() => {}}/> 
+          <Collapse isOpen = {false}navbar >
+          <Nav className = "ml-auto" navbar>
+          {/* <NavItem>
+               <NavLink href ="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+               <NavLink href ="https://github.com/reactstrap/reactstrap">
+               Shipments
+               </NavLink>
+               
+          </NavItem> */}
+          <NavItem>
+               <NavLink href ="/components/">Plan Your Trip</NavLink>
+          </NavItem>
+          <NavItem>
+               <NavLink href ="https://github.com/reactstrap/reactstrap">
+               What Time Does the Next Bus Come?
+               </NavLink>
+           </NavItem>
+          </Nav> 
+          </Collapse>
+          </Navbar>
+
+          </div>
+     )
+}
+
+export default Header;
